482d7defdbb2cc04c9be9cf9f61a58c4cd4438f4
christine928 <christine.lefevre2@gmail.com>
2019-02-22 14:26:37 +0100

    minimisation

--- projet.h
+++ projet.h
@@ -1,5 +1,10 @@
 #ifndef PROJET_H
 #define PROJET_H
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>//permet de manipuler les chaines de caracteres
+
 
 	typedef struct
 	{
@@ -34,5 +39,27 @@
 		int tailleAccept;
 		
 	}AFD;
+
+
+AFN langage_vide(); //creer un alphabet=> AFN  //lea
+AFN mot_vide(); //ajouter a un alphabet le mot vide //lea
+AFN mot(char caractere); //chr//fait
+
+
+AFN reunion (AFN afn1, AFN afn2);//chr//a voir avec la concat
+AFN concat (AFN afn1, AFN afn2);//lea enleve des etats accepteurs
+AFN etoile (AFN afn);//lea
+
+_Bool exec_automate(AFD afd, char * mot);//chr
+AFD determinisation(AFN afn);//verifier langage identique //lea
+AFD minimisation (AFD afd); //chr
+
+
+//nos fonctions
+void aff_AFN (AFN * AF, int taille);//type vaut false si AFN, true pour AFD
+void aff_AFD( AFD * AF, int taille);//type vaut false si AFN, true pour AFD
+int choix(int taille);
+int * remplir_entete_2(AFD afd, int ** tab, int nbreCol, int nbreLigne, int * entete, int * entete2, int * cpt);
+
 
 #endif
